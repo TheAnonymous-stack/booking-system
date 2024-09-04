702aaebf16646bcb5d84982cfaa4b977d984109b
TheAnonymous-stack <thanhnht.2014@gmail.com>
2024-09-04 11:36:39 -0400

    updated firebaseConfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2vvWEddO_qzlWVOyRrA9PvToN91z6BMk",
-  authDomain: "booking-system-f3c68.firebaseapp.com",
-  projectId: "booking-system-f3c68",
-  storageBucket: "booking-system-f3c68.appspot.com",
-  messagingSenderId: "863060471168",
-  appId: "1:863060471168:web:bf8e5bab627ceeacf72565",
-  measurementId: "G-6KGGHJRP1C"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 // Initialize Firebase
